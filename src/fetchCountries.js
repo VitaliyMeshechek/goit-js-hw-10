16bec3cac692798eee658daaea11b13587c11643
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-11-22 20:01:11 +0200

    11

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https:restcountries.com/v3.1/name/';
 const FIELDS = 'name,capital,languages,flags,population';
 
 export default function fetchCountries(name){
-  return fetch(`${BASE_URL}${name}?fields=${FIELDS}`).then(response => {
+  return fetch(`${BASE_URL}${name}?fields=name,capital,languages,flags,population`).then(response => {
   if(!response.ok){
       throw new Error(response.statusText);
     }
