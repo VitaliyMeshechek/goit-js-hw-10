68772400e503b4736f4fd6a2dec933db1d304ba8
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-11-23 11:59:47 +0200

    111

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 
-const BASE_URL = 'https:restcountries.com/v3.1/name/';
-const FIELDS = 'name,capital,languages,flags,population';
+const BASE_URL = `https:restcountries.com/v3.1/name/`;
+const FIELDS = `name,capital,languages,flags,population`;
 
 export default function fetchCountries(name){
   return fetch(`${BASE_URL}${name}?fields=${FIELDS}`).then(response => {
